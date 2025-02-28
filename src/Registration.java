b90cedf28971872dbf2baa65fbd242a85b8c7fc3
MatteoPostiferi04 <matteo.postiferi@tuni.fi>
2025-02-28 18:59:05 +0100

    Registration class created

---
+++ src/Registration.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Registration {
+    public static User registerUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter your email:");
+        String email = scanner.nextLine();
+        System.out.println("Enter your password:");
+        String password = scanner.nextLine();
+        System.out.println("Are you a Traveler or a Guide? (T/G)");
+        String role = scanner.nextLine().toUpperCase();
+
+        boolean hasLicense = false;
+        if (role.equals("G")) {
+            System.out.println("Do you have a guide license? (true/false)");
+            hasLicense = Boolean.parseBoolean(scanner.nextLine());
+        }
+
+        try {
+            return UserFactoy.UserFactory.createUser(name, email, password, role, hasLicense);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
+
