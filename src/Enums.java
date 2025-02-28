e13169922ccc123f514b509bd97c82534acd030f
MatteoPostiferi04 <matteo.postiferi@tuni.fi>
2025-02-28 17:24:36 +0100

    Preference, Skill and PaymentStatus enums created

---
+++ src/Enums.java
@@ -0,0 +1,14 @@
+public class Enums {
+
+    public enum Preference {
+        ADVENTURE, RELAX, CULTURE
+    }
+
+    public enum Skill {
+        HIKING, HISTORY_KNOWLEDGE, SCUBA_DIVING
+    }
+
+    public enum PaymentStatus {
+        PENDING, COMPLETED, CANCELLED
+    }
+}
