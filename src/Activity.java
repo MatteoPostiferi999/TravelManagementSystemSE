02d923b62d09c7ac41ee913ff7c26c5f205965cd
MatteoPostiferi04 <matteo.postiferi@tuni.fi>
2025-02-28 17:23:34 +0100

    Trip and Activity classes created

---
+++ src/Activity.java
@@ -0,0 +1,21 @@
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Activity {
+    private String id;
+    private String name;
+    private String description;
+    private LocalDateTime time;
+    private String location;
+    private List<Guide> guides;
+
+    public Activity(String id, String name, String description, LocalDateTime time, String location, List<Guide> guides) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.time = time;
+        this.location = location;
+        this.guides = guides;
+    }
+}
+
---
+++ src/Trip.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Trip {
+    private String id;
+    private String destination;
+    private List<Activity> activities;
+    private int availableSpots;
+    private double price;
+    private Guide assignedGuide;
+    private List<Enums.Skill> requiredSkills;
+
+    public Trip(String id, String destination, List<Activity> activities, int availableSpots, double price) {
+        this.id = id;
+        this.destination = destination;
+        this.activities = activities;
+        this.availableSpots = availableSpots;
+        this.price = price;
+    }
+    public List<Enums.Skill> getRequiredSkills() {
+        return requiredSkills;
+    }
+
+}
